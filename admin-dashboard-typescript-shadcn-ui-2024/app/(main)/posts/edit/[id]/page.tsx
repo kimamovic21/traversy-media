c71dabcf888753c30c3372751e50fa2d76a4c3f6
kimamovic21 <imamovic.kerim@gmail.com>
2024-07-30 16:01:20 +0200

    dodana logika za paginaciju
    -kreiran folder posts i komponenta page.tsx
    -kreiran folder edit, [id] i komponenta page.tsx
    -kreirana komponenta BackButton.tsx
    -u folderu posts kreirana komponenta PostsPagination.tsx
    -u folderu ui generisane nove shadcn-ui tsx komponente
    -instalirani novi paketi u terminalu

---
+++ admin-dashboard-typescript-shadcn-ui-2024/app/(main)/posts/edit/[id]/page.tsx
@@ -0,0 +1,11 @@
+import BackButton from "@/components/BackButton"
+
+const PostsEditPage = () => {
+  return (
+    <>
+        <BackButton text="Back To Posts" link="/posts" />
+    </>
+  )
+}
+
+export default PostsEditPage

75b5618a58694b1507eeec8627d333848da00fb9
kimamovic21 <imamovic.kerim@gmail.com>
2024-07-31 17:43:00 +0200

    manja izmjena u (auth) layout.tsx komponenti
    -u komponentu layout.tsx dodana komponenta ThemeToggler

--- admin-dashboard-typescript-shadcn-ui-2024/app/(auth)/layout.tsx
+++ admin-dashboard-typescript-shadcn-ui-2024/app/(auth)/layout.tsx
@@ -1,9 +1,14 @@
-const AuthLayout = ({ children }: {children: React.ReactNode }) => {
-  return (
-    <div className="h-[100vh] flex flex-items items-center justify-center relative">
-        {children}
-    </div>
-  )
-}
+import ThemeToggler from '@/components/ThemeToggler';
 
-export default AuthLayout
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className='h-[100vh] flex items-center justify-center relative'>
+      <div className='absolute bottom-5 right-0 text-white'>
+        <ThemeToggler />
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default AuthLayout;

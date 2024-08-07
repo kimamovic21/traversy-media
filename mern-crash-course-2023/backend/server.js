3b416e6eea582885e43a214810018b37b2d38391
kimamovic21 <imamovic.kerim@gmail.com>
2024-08-07 16:09:30 +0200

    MongoDB i model User
    -u .env fajl dodana varijabla MONGO_URI
    -kreiran folder config i db.js fajl
    -kreiran folder models i userModel.js fajl
    -manje izmjene u server.js fajlu

--- mern-crash-course-2023/backend/server.js
+++ mern-crash-course-2023/backend/server.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 import userRoutes from './routes/userRoutes.js';
 
 
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 

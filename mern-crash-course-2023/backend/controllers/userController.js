b88fe9bb750c61b1dafcb4e67d26073d7b3523e7
kimamovic21 <imamovic.kerim@gmail.com>
2024-08-12 11:45:00 +0200

    Logout & Destroy Cookie
    -u userController.js fajl dodana logika u funkciju logoutUser()

--- mern-crash-course-2023/backend/controllers/userController.js
+++ mern-crash-course-2023/backend/controllers/userController.js
@@ -64,7 +64,11 @@ export const registerUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users/logout
 // @access  Public
 export const logoutUser = (req, res) => {
-    res.status(200).json({ message: 'Logout User' });
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: 'Logged out successfully' });
 };
 
 

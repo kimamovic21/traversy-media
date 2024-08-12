13b843523c4506ccb922c0b30dbe1946b9423489
kimamovic21 <imamovic.kerim@gmail.com>
2024-08-12 11:42:14 +0200

    Auth User Endpoint
    -u userController.js fajlu dodana logika u funkciju authUser()

--- mern-crash-course-2023/backend/controllers/userController.js
+++ mern-crash-course-2023/backend/controllers/userController.js
@@ -7,7 +7,22 @@ import User from '../models/userModel.js';
 // @route   POST /api/users/auth
 // @access  Public
 export const authUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Auth User' });    
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
 });
 
 

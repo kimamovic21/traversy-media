a6aa8ed888a18d56aa4b822ab6c2840569e27269
kimamovic21 <imamovic.kerim@gmail.com>
2024-08-12 11:35:48 +0200

    Generate JWT & Save Cookie
    -kreiran folder utils i generateToken.js fajl
    -u userController.js fajl dodana funkcija generateToken()
    -manja izmjena u userModel.js fajlu
    -u fajlovima .env i env.example kreirana varijabla JWT_SECRET

--- mern-crash-course-2023/backend/controllers/userController.js
+++ mern-crash-course-2023/backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler';
-import User from '../models/userModel';
+import generateToken from '../utils/generateToken.js';
+import User from '../models/userModel.js';
 
 
 // @desc    Auth user & get token

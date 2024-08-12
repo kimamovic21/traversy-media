82cfb0bd919d2bac9b8ad32605af6fc561b3694d
kimamovic21 <imamovic.kerim@gmail.com>
2024-08-12 11:27:07 +0200

    Register user endpoint
    -u userController.js fajlu dodana logika za funkciju registerUser()
    -u userModel.js fajlu dodana logika za enkripciju korisnikove lozinke
    -u server.js fajlu kreirane dvije nove metode app.use() za json i urlencoded

--- mern-crash-course-2023/backend/controllers/userController.js
+++ mern-crash-course-2023/backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler';
+import User from '../models/userModel';
 
 
 // @desc    Auth user & get token
@@ -13,7 +14,33 @@ export const authUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users
 // @access  Public
 export const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Register User' });
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    generateToken(res, user._id);
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
 });
 
 

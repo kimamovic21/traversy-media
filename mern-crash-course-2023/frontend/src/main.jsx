84885c179574c5eb472e22546181818661e67e05
kimamovic21 <imamovic.kerim@gmail.com>
2024-08-13 17:26:14 +0200

    Private Route Component
    -u folderu components kreirana komponenta PrivateRoute.jsx
    -u folderu screens kreirana komponenta ProfileScreen.jsx
    -u komponentu main.jsx dodane komponente ProfileScreen.jsx i PrivateRoute.jsx
    -u komponenti main.jsx kreirana zasticena ruta za komponentu ProfileScreen

--- mern-crash-course-2023/frontend/src/main.jsx
+++ mern-crash-course-2023/frontend/src/main.jsx
@@ -10,13 +10,18 @@ import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } 
 import HomeScreen from './screens/HomeScreen.jsx'
 import LoginScreen from './screens/LoginScreen.jsx'
 import RegisterScreen from './screens/RegisterScreen.jsx'
+import PrivateRoute from './components/PrivateRoute.jsx'
+import ProfileScreen from './screens/ProfileScreen.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<HomeScreen />} />
-      <Route path='/login' element={<LoginScreen />} /> 
+      <Route path='/login' element={<LoginScreen />} />
       <Route path='/register' element={<RegisterScreen />} />
+      <Route path='' element={<PrivateRoute />}>
+        <Route path='/profile' element={<ProfileScreen />} />
+      </Route>
     </Route>
   )
 )

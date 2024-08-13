c1cfd75bd56976c3f505c06a456bb5cbf977bd4c
kimamovic21 <imamovic.kerim@gmail.com>
2024-08-13 14:50:52 +0200

    Login & Register Form UIs
    -u .eslintrc.cjs fajl dodana iskljucena opcija za Prop Types
    -u folderu components kreirana komponenta FormContainer.jsx
    -u folderu screens kreirane komponente LoginScreen.jsx i RegisterScreen.jsx
    -u komponentu main.jsx dodane komponente LoginScreen.jsx i RegisterScreen.jsx
    -kreirane nove rute za komponente LoginScreen i RegisterScreen

--- mern-crash-course-2023/frontend/src/main.jsx
+++ mern-crash-course-2023/frontend/src/main.jsx
@@ -4,12 +4,17 @@ import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+
 import HomeScreen from './screens/HomeScreen.jsx'
+import LoginScreen from './screens/LoginScreen.jsx'
+import RegisterScreen from './screens/RegisterScreen.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/login' element={<LoginScreen />} /> 
+      <Route path='/register' element={<RegisterScreen />} />
     </Route>
   )
 )

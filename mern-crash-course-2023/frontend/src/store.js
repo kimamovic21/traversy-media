4d8d4a1292a09713abaa7cae9dc1c258f0ebb193
kimamovic21 <imamovic.kerim@gmail.com>
2024-08-13 15:36:05 +0200

    API Slices
    -u folderu slices kreirani fajlovi apiSlice.js i usersApiSlice.js
    -u store.js fajl dodan objekt apiSlice

--- mern-crash-course-2023/frontend/src/store.js
+++ mern-crash-course-2023/frontend/src/store.js
@@ -1,12 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './slices/authSlice';
+import { apiSlice } from './slices/apiSlice';
+import authReducer from './slices/authSlice'; 
 
 const store = configureStore({
   reducer: {
-    auth: authReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer, 
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
-  devTools: true,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+    devTools: true,
 });
 
 export default store;

83c1c510e442d2a6a31f4510d95ab0fc9dff71fb
kimamovic21 <imamovic.kerim@gmail.com>
2024-08-13 14:13:14 +0200

    HomeScreen & Hero
    -u folderu components kreirana komponenta Hero.jsx
    -u folder src kreiran folder screens i komponenta HomeScreen.jsx
    -u komponentu App.jsx dodana komponenta HomeScreen.jsx

--- mern-crash-course-2023/frontend/src/App.jsx
+++ mern-crash-course-2023/frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import Header from "./components/Header"
+import HomeScreen from "./screens/HomeScreen"
 
 const App = () => {
   return (
     <div>
       <Header />
-      <h1>MERN Crash Course 2023</h1>
+      <HomeScreen />
     </div>
   )
 }

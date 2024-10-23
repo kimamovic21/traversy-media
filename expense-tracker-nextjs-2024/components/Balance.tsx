4a8cda130f81a3e391e71e3dec128bad659923b4
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-23 15:21:07 +0200

    getUserBalance Action
    -u folderu actions kreiramo getUserBalance.ts fajl
    -u getUserBalance.ts fajlu kreiramo i eksportujemo asinhronu funkciju getUserBalance()
    -u komponentu Balance.tsx dodajemo funkciju getUserBalance

--- expense-tracker-nextjs-2024/components/Balance.tsx
+++ expense-tracker-nextjs-2024/components/Balance.tsx
@@ -1,8 +1,12 @@
-const Balance = () => {
+import getUserBalance from '@/app/actions/getUserBalance';
+
+const Balance = async () => {
+  const { balance } = await getUserBalance();
+
   return (
     <>
       <h4>Your Balance</h4> 
-      <h1>$500</h1>
+      <h1>${balance ?? 0}</h1>
     </>
   );
 };

a78900eee4fa952cf990856cf09035e65b9754c5
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-24 16:23:08 +0200

    Delete Button & deleteTransaction Action
    -u folderu actions kreiran deleteTransaction.ts fajl
    -u deleteTransaction.ts fajlu kreirana i eksportovana funkcija  deleteTransaction() za brisanje transakcija
    -u komponentu TransactionItem.tsx dodana funkcija deleteTransaction i logika za brisanje transakcija

--- expense-tracker-nextjs-2024/components/TransactionItem.tsx
+++ expense-tracker-nextjs-2024/components/TransactionItem.tsx
@@ -1,18 +1,39 @@
 'use client';
 
+import { toast } from 'react-toastify';
 import { addCommas } from '@/lib/utils';
 import { Transaction } from '@/types/Transaction';
+import deleteTransaction from '@/app/actions/deleteTransaction';
 
 const TransactionItem = ({ transaction } : { transaction: Transaction }) => {
-    const sign = transaction.amount < 0 ? '-' : '+';
-  
-    return (
-    
+  const sign = transaction.amount < 0 ? '-' : '+';
+
+  const handleDeleteTransaction = async (transactionId: string) => {
+    const confirmed = window.confirm('Are you sure you want to delete this transaction?');
+
+    if (!confirmed) return;
+
+    const { message, error } = await deleteTransaction(transactionId);
+
+    if (error) {
+      toast.error(error);
+    };
+
+    toast.success(message);
+  };
+
+  return (
     <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
       {transaction.text}
       <span>
         {sign}${addCommas(Math.abs(transaction.amount))}
       </span>
+      <button
+        onClick={() => handleDeleteTransaction(transaction.id)}
+        className='delete-btn'
+      >
+        x
+      </button>
     </li>
   );
 };

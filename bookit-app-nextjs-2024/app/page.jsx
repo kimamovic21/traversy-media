b875e3d49877e790975d4323043a24dd5640cf96
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-25 15:04:13 +0200

    RoomCard Component
    -kreiran folder public i images sa slikama prostorija
    -u folderu components kreirana komponenta RoomCard.jsx
    -u komponenti RoomCard.jsx, funkciji RoomCard() dodan prop room za prikazivanje podataka o prostorijama
    -u komponentu RoomCard.jsx dodane komponente Image i Link iz paketa Next
    -u page.jsx fajl dodana komponenta RoomCard.jsx

--- bookit-app-nextjs-2024/app/page.jsx
+++ bookit-app-nextjs-2024/app/page.jsx
@@ -1,11 +1,17 @@
 import rooms from '@/data/rooms.json';
+import RoomCard from '@/components/RoomCard';
 
 export default function Home() {
   return (
     <>
       <h1>Bookit App</h1>
       {rooms.length > 0 ? (
-        rooms.map((room) => <h3>{room.name}</h3>)
+        rooms.map((room) => 
+          <RoomCard 
+            key={room?.$id}
+            room={room} 
+          />
+        )
       ) : (
         <p>No rooms available at the moment...</p>
       )}

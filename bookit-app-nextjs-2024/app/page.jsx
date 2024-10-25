1cf1bb06ac7a7948434b942dfb358fe2f1a3876f
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-25 15:09:19 +0200

    Heading Component
    -u folderu components kreirana komponenta Heading.jsx
    -u page.jsx fajl dodana komponenta Heading.jsx

--- bookit-app-nextjs-2024/app/page.jsx
+++ bookit-app-nextjs-2024/app/page.jsx
@@ -1,10 +1,11 @@
 import rooms from '@/data/rooms.json';
 import RoomCard from '@/components/RoomCard';
+import Heading from '@/components/Heading';
 
 export default function Home() {
   return (
     <>
-      <h1>Bookit App</h1>
+      <Heading title='Available Rooms' />
       {rooms.length > 0 ? (
         rooms.map((room) => 
           <RoomCard 

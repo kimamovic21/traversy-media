e930d5c624cc98a3283d85dd45853111c1caddb1
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-29 16:12:03 +0100

    Add Room
    -u folderu actions kreiran createRoom.js fajl
    -u createRoom.js fajlu kreirana i eksportovana asinhrona funkcija createRoom() za kreiranje prostorija
    -u folderu rooms kreiran folders add i page.jsx fajl
    -u add/page.jsx fajl dodane funkcije useEffect, useFormState, useRouter, toast i createRoom
    -u add/page.jsx fajl dodana logika za kreiranje prostorija

---
+++ bookit-app-nextjs-2024/app/actions/createRoom.js
@@ -0,0 +1,54 @@
+'use server';
+
+import { ID } from 'node-appwrite';
+import { revalidatePath } from 'next/cache';
+import { createAdminClient } from '@/config/appwrite';
+import checkAuth from './checkAuth';
+
+async function createRoom(previousState, formData) {
+  // Get databases instance
+  const { databases } = await createAdminClient();
+
+  try {
+    const { user } = await checkAuth();
+
+    if (!user) {
+      return {
+        error: 'You must be logged in to create a room'
+      };
+    };
+
+    // Create room
+    const newRoom = await databases.createDocument(
+        process.env.NEXT_PUBLIC_APPWRITE_DATABASE,
+        process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ROOMS,
+        ID.unique(),
+        {
+            user_id: user.id,
+            name: formData.get('name'),
+            description: formData.get('description'),
+            sqft: formData.get('sqft'),
+            capacity: formData.get('capacity'),
+            location: formData.get('location'),
+            address: formData.get('address'),
+            availability: formData.get('availability'),
+            price_per_hour: formData.get('price_per_hour'),
+            amenities: formData.get('amenities'),
+        }
+    );
+
+    revalidatePath('/', 'layout');
+
+    return {
+      success: true    
+    };
+  } catch (error) {
+    console.error(error);
+    const errorMessage = error.response.message || 'An unexpected error has occurred'
+    return {
+      error: errorMessage  
+    }
+  };
+};
+
+export default createRoom;

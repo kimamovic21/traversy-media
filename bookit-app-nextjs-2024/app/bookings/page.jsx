5c03a702a5783fc263686fe4f3f69cb6d34c0a57
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-30 15:05:43 +0100

    Show Bookings
    -u folderu actions kreiran getMyBookings.js fajl
    -u getMyBookings.js fajlu kreirana i eksportovana asinhrona funkcija getMyBookings() za dohvacanje rezervacija prostorija
    -u bookings/page.jsx fajl dodana funkcija getMyBookings

--- bookit-app-nextjs-2024/app/bookings/page.jsx
+++ bookit-app-nextjs-2024/app/bookings/page.jsx
@@ -1,6 +1,18 @@
-const BookingsPage = () => {
+import getMyBookings from '../actions/getMyBookings';
+
+const BookingsPage = async () => {
+  const bookings = await getMyBookings();
+
   return (
-    <div>BookingsPage</div>
+    <>
+      {bookings.length === 0 ? (
+        <p className='text-gray-600 mt-4'>You have no bookings</p>
+      ) : (
+        bookings.map((booking) => 
+          <h3>{booking.room_id.name}</h3>
+        )
+      )}
+    </>
   );
 };
 

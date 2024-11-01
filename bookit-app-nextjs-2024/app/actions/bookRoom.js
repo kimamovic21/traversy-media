8a6f5759d7a76ed9aa34b23373e474cc274456c6
kimamovic21 <imamovic.kerim@gmail.com>
2024-11-01 11:05:45 +0100

    Check Room Availability
    -u terminalu instaliran paket Luxon
    -u folderu actions kreiran checkRoomAvailability.js fajl
    -u checkRoomAvailability.js fajlu kreirana i eksportovana asinhrona funkcija checkRoomAvailability() za provjeru rezervacije prostorije
    -u bookRoom.js fajl dodana funkcija checkRoomAvailability

--- bookit-app-nextjs-2024/app/actions/bookRoom.js
+++ bookit-app-nextjs-2024/app/actions/bookRoom.js
@@ -6,12 +6,13 @@ import { revalidatePath } from 'next/cache';
 import { ID } from 'node-appwrite';
 import { createSessionClient } from '@/config/appwrite';
 import checkAuth from './checkAuth';
+import checkRoomAvailability from './checkRoomAvailability';
 
 async function bookRoom(previousState, formData) {
   const sessionCookie = cookies().get('appwrite-session');
-  
+
   if (!sessionCookie) {
-    redirect('/');
+    redirect('/login');
   };
 
   try {
@@ -19,27 +20,42 @@ async function bookRoom(previousState, formData) {
 
     // Get user's ID
     const { user } = await checkAuth();
+
     if (!user) {
       return {
-        error: 'You must be logged in to book a room'
+        error: 'You must be logged in to book a room',
       };
-    };
+    }
 
     // Extract date and time from the formData
     const checkInDate = formData.get('check_in_date');
     const checkInTime = formData.get('check_in_time');
     const checkOutDate = formData.get('check_out_date');
     const checkOutTime = formData.get('check_out_time');
+    const roomId = formData.get('room_id');
 
     // Combine date and time to ISO 8601 format
     const checkInDateTime = `${checkInDate}T${checkInTime}`;
     const checkOutDateTime = `${checkOutDate}T${checkOutTime}`;
 
+    // Check if room is available
+    const isAvailable = await checkRoomAvailability(
+      roomId,
+      checkInDateTime,
+      checkOutDateTime
+    );
+
+    if (!isAvailable) {
+      return {
+        error: 'This room is already booked for the selected time',
+      };
+    };
+
     const bookingData = {
       check_in: checkInDateTime,
       check_out: checkOutDateTime,
       user_id: user.id,
-      room_id: formData.get('room_id')
+      room_id: roomId,
     };
 
     // Create booking
@@ -54,12 +70,12 @@ async function bookRoom(previousState, formData) {
     revalidatePath('/bookings', 'layout');
 
     return {
-      success: true  
+      success: true
     };
   } catch (error) {
     console.error(error);
     return {
-      error: 'Something went wrong booking the room'
+      error: 'Something went wrong booking the room',
     };
   };
 };

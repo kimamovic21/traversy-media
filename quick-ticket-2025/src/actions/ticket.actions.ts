df561140eb8376bb89053ea40959dcb1305b2821
Kerim Imamović <imamovic.kerim@gmail.com>
2025-05-17 14:31:16 +0200

    New ticket form & submission

---
+++ quick-ticket-2025/src/actions/ticket.actions.ts
@@ -0,0 +1,59 @@
+'use server';
+import { revalidatePath } from 'next/cache';
+import { prisma } from '@/db/prisma';
+import { logEvent } from '@/utils/sentry'; 
+
+export async function createTicket(
+  prevState: { success: boolean; message: string },
+  formData: FormData
+): Promise<{ success: boolean; message: string }> {
+  try {
+    const subject = formData.get('subject') as string;
+    const description = formData.get('description') as string;
+    const priority = formData.get('priority') as string;
+
+    if (!subject || !description || !priority) {
+      logEvent(
+        'Validation Error: Missing ticket fields',
+        'ticket',
+        { subject, description, priority },
+        'warning'
+      );
+      return { success: false, message: 'All fields are required' };
+    }
+
+    // Create ticket
+    const ticket = await prisma.ticket.create({
+      data: { subject, description, priority },
+    });
+
+    logEvent(
+      `Ticket created successfully: ${ticket.id}`,
+      'ticket',
+      { ticketId: ticket.id },
+      'info'
+    );
+
+    revalidatePath('/tickets');
+
+    return {
+      success: true,
+      message: 'Ticket created successfully'
+    };
+  } catch (error) {
+    logEvent(
+      'An error occured while creating the ticket',
+      'ticket',
+      {
+        formData: Object.fromEntries(formData.entries()),
+      },
+      'error',
+      error
+    );
+
+    return {
+      success: false,
+      message: 'An error occured while creating the ticket',
+    };
+  };
+};
--- quick-ticket-2025/src/app/tickets/new/page.tsx
+++ quick-ticket-2025/src/app/tickets/new/page.tsx
@@ -1,6 +1,10 @@
+import NewTicketForm from './ticket-form';
+
 const NewTicketPage = () => {
   return (
-    <div>New Ticket Page</div>
+    <div className='min-h-screen bg-blue-50 flex items-center justify-center px-4'>
+      <NewTicketForm />
+    </div>
   );
 };
 

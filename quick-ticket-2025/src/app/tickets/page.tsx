46e124e98c7a8b7a8e1422b96dc102296bddf714
Kerim Imamović <imamovic.kerim@gmail.com>
2025-05-19 17:16:28 +0200

    TicketItem component

--- quick-ticket-2025/src/app/tickets/page.tsx
+++ quick-ticket-2025/src/app/tickets/page.tsx
@@ -1,8 +1,7 @@
 import { redirect } from 'next/navigation';
-import { getTickets } from '@/actions/ticket.actions';
-import { getPriorityClass } from '@/utils/ui';
 import { getCurrentUser } from '@/lib/current-user';
-import Link from 'next/link';
+import { getTickets } from '@/actions/ticket.actions';
+import TicketItem from '@/components/TicketItem';
 
 const TicketsPage = async () => {
   const user = await getCurrentUser();
@@ -26,33 +25,7 @@ const TicketsPage = async () => {
       ) : (
         <div className='space-y-4 max-w-3xl mx-auto'>
           {tickets?.map((ticket) => (
-            <div
-              key={ticket.id}
-              className='flex justify-between items-center bg-white rounded-lg shadow border border-gray-200 p-6'
-            >
-              {/* Left Side */}
-              <div>
-                <h2 className='text-xl font-semibold text-blue-600'>
-                  {ticket.subject}
-                </h2>
-              </div>
-
-              {/* Right Side */}
-              <div className='text-right space-y-2'>
-                <div className='text-sm text-gray-500'>
-                  Priority:{' '}
-                  <span className={getPriorityClass(ticket.priority)}>
-                    {ticket.priority}
-                  </span>
-                </div>
-                <Link
-                  href={`/tickets/${ticket.id}`}
-                  className='inline-block mt-2 bg-blue-600 text-white text-sm px-3 py-1 rounded hover:bg-blue-700 transition text-center'
-                >
-                  View Ticket
-                </Link>
-              </div>
-            </div>
+            <TicketItem key={ticket.id} ticket={ticket} />
           ))}
         </div>
       )}
